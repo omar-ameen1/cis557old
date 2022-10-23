8a0f11609ca8b76264844d1bd12639eb0af907b8
Omar <omario@sas.upenn.edu>
2022-10-23 16:28:38 -0400

    made header

--- webgram/src/App.js
+++ webgram/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header-components/header";
+import MainBody from "./components/main-view-components/main-body";
+import { } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import './css/main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Header searchIcon={faMagnifyingGlass}/>
+        <MainBody />
+      </>
+
   );
 }
 
